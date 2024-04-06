0cabda7f6dfac44ede5d9576bc22641d454fd184
alfie <alfieroddan@gmail.com>
2024-04-06 13:45:16 +0100

    longer wait time for images

--- opencv_calibrate/calibrate.py
+++ opencv_calibrate/calibrate.py
@@ -69,7 +69,7 @@ def calibrate_numpy(array: np.array,
             if DEBUG > 0:
                 cv2.drawChessboardCorners(frame, (p_r, p_c), corners2, ret)
                 cv2.imshow('img', frame)
-                cv2.waitKey(1)
+                cv2.waitKey(500)
     cv2.destroyAllWindows()
     cv2.waitKey(1)
 
